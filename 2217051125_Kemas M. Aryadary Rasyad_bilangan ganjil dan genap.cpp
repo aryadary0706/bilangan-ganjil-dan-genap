d9d4e5b8ebff8ce77d299dd13d8ce614a4b6fdf9
aryadary0706 <112590882+aryadary0706@users.noreply.github.com>
2022-09-27 21:17:13 +0700

    Add files via upload

---
+++ 2217051125_Kemas M. Aryadary Rasyad_bilangan ganjil dan genap.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int nilai;
+	cin>>nilai;
+	if(nilai%2==0){
+		cout<<"Bilangan tersebut bilangan genap";
+	}else if(nilai%2==1){
+		cout<<"bilangan tersebut bilangan ganjil";
+	}
+}
